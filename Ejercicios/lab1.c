bf5ee65e823db10c1029af82db262fcbca2f59c6
fede <federico.cechich@mi.unc.edu.ar>
2024-08-23 00:36:08 -0300

    lab1

--- Ejercicios/lab1.c
+++ Ejercicios/lab1.c
@@ -1,4 +1,18 @@
+
+/**
+ * @file lab1.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
 #include <stdio.h>
+
 
 typedef union {
     int int_data;
